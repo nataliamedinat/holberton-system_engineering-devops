77f8613325cea56ba89d2a45a07e7f42a99c00a4
nataliamedinat <nataliamedinabotero@hotmail.com>
2020-02-04 04:59:38 +0000

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,11 +6,11 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    employee_id = argv[1]
+    id_employee = argv[1]
     url_todo = 'https://jsonplaceholder.typicode.com/todos/'
     url_user = 'https://jsonplaceholder.typicode.com/users/'
-    todo = requests.get(url_todo, params={'userId': employee_id})
-    user = requests.get(url_user, params={'id': employee_id})
+    todo = requests.get(url_todo, params={'userId': id_employee})
+    user = requests.get(url_user, params={'id': id_employee})
 
     todo_dict_list = todo.json()
     user_dict_list = user.json()
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     user_tasks = {}
     employee = user_dict_list[0].get('username')
 
-    with open("{}.json".format(employee_id), "w+") as jsonfile:
+    with open("{}.json".format(id_employee), "w+") as jsonfile:
         for task in todo_dict_list:
             status = task.get('completed')
             title = task.get('title')
@@ -27,7 +27,7 @@ if __name__ == '__main__':
             task_dict['completed'] = status
             task_dict['username'] = employee
             task_list.append(task_dict)
-        user_tasks[employee_id] = task_list
+        user_tasks[id_employee] = task_list
 
         data = json.dumps(user_tasks)
         jsonfile.write(data)
