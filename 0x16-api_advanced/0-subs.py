f16e88086882289f341ea35e8217d61dd673d561
nataliamedinat <nataliamedinabotero@gmail.com>
2020-02-07 02:34:59 +0000

    Get the number of subscribers of a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" """
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """ Get the number os subscribers of a given subreddit """
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    request = requests.get(url, headers=headers)
+    req = request.json()
+
+    if request.status_code == 404:
+        return 0
+
+    subs = req.get('data').get('subscribers')
+    return subs
