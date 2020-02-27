982ae264584e27750e1dc466a1b05eb87db297ae
nataliamedinat <nataliamedinabotero@gmail.com>
2020-02-27 21:56:45 +0000

    Script

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
 # Increases worker_processes to nginx conf file
 exec { 'Increase_workers':
-  command  => "sed -i 's/worker_processes 4;/worker_processes 100;/g'/etc/nginx/nginx.conf && sudo service nginx restart",
-  provider => 'shell',
+  command  => "sed -i 's/worker_processes 4;/worker_processes 100;/g'
+  /etc/nginx/nginx.conf && sudo service nginx restart",
+    provider => 'shell',
 }
